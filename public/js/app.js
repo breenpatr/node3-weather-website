4e88e4d2eb49749e9777de7c5f28bbac0a2273e4
NHODOM01\joh8994 <patrick.breen@johnson.ca>
2021-04-14 13:29:15 -0230

    remove unnecessary console.log cacll

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,6 @@
 
 //this is the client side javascript. 
 
-console.log('Client side javascript is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
